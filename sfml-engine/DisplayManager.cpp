f7dedfd47023e4b022dd5f1c765388de10a8b149
kevin-blanchet <kevin.blanchet@ynov.com>
2022-04-12 14:18:56 +0200

    Added display manager

---
+++ sfml-engine/DisplayManager.cpp
@@ -0,0 +1,113 @@
+#include "DisplayManager.h"
+#include "GameManager.h"
+#include "LogManager.h"
+
+DisplayManager::~DisplayManager()
+{
+	this->pWindow->close();
+	delete this->pWindow;
+}
+
+DisplayManager& DisplayManager::getInstance()
+{
+	static DisplayManager displayManager;
+	return displayManager;
+}
+
+bool DisplayManager::drawString(sf::Vector2f pos, std::string str, Justification just, sf::Color color) const
+{
+	if (pWindow == NULL) return false;
+
+	sf::Text text(str, this->defaultFont);
+	text.setFillColor(color);
+
+	sf::Vector2f startingPos = pos;
+	switch (just)
+	{
+	case Justification::Center:
+		startingPos.x = pos.x - text.getGlobalBounds().width / 2;
+		break;
+	case Justification::Right:
+		startingPos.x = pos.x - text.getGlobalBounds().width;
+		break;
+	case Justification::Left:
+	default:
+		break;
+	}
+	text.setPosition(startingPos);
+	this->pWindow->draw(text);
+	return true;
+}
+
+void DisplayManager::update()
+{
+	sf::Event event;
+	while (pWindow->pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed) {
+			pWindow->close();
+			GM.setRunning(false);
+		}
+	}
+	pWindow->draw(testCircle);
+
+
+
+
+
+
+	if (this->pWindow == NULL) LM.consoleLog("Err: No Window");
+	pWindow->display();
+	pWindow->clear(this->bgColor);
+}
+
+int DisplayManager::getWidth() const
+{
+	return this->windowWidth;
+}
+
+int DisplayManager::getHeight() const
+{
+	return this->windowHeight;
+}
+
+sf::Color DisplayManager::getBgColor() const
+{
+	return this->bgColor;
+}
+
+void DisplayManager::setBgColor(sf::Color newColor)
+{
+	this->bgColor = newColor;
+}
+
+bool DisplayManager::setDefaultFont(std::string fontFile)
+{
+	return this->defaultFont.loadFromFile(fontFile);
+}
+
+sf::RenderWindow* DisplayManager::getWindow() const
+{
+	return this->pWindow;
+}
+
+DisplayManager::DisplayManager()
+{
+	this->setType("DisplayManager");
+	this->windowWidth = WINDOW_WIDTH_DEFAULT;
+	this->windowHeight = WINDOW_HEIGHT_DEFAULT;
+	this->bgColor = WINDOW_BG_COLOR_DEFAULT;
+
+	this->pWindow = new sf::RenderWindow(sf::VideoMode(this->windowWidth, this->windowHeight), WINDOW_TITLE_DEFAULT, WINDOW_STYLE_DEFAULT);
+	this->pWindow->setVerticalSyncEnabled(WINDOW_VSYNC_DEFAULT);
+	this->pWindow->setFramerateLimit(WINDOW_FRAMERATE_DEFAULT);
+
+	if (!this->defaultFont.loadFromFile(DEFAULT_FONT_FILE)) {
+		LM.errorLog("failed to load font");
+	}
+
+	this->update();
+
+	this->testCircle = sf::CircleShape(100.f);
+	this->testCircle.setFillColor(sf::Color::Green);
+}
---
+++ sfml-engine/DisplayManager.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Manager.h"
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+#define DM DisplayManager::getInstance()
+
+
+enum class Justification
+{
+    Left,
+    Center,
+    Right
+};
+
+const std::string DEFAULT_FONT_FILE = "Ressources/Fonts/ARIAL.TTF";
+
+const int WINDOW_FRAMERATE_DEFAULT = 60;
+const bool WINDOW_VSYNC_DEFAULT = true;
+const int WINDOW_WIDTH_DEFAULT = 1280;
+const int WINDOW_HEIGHT_DEFAULT = 720;
+const int WINDOW_STYLE_DEFAULT = sf::Style::Titlebar | sf::Style::Close;
+const sf::Color WINDOW_BG_COLOR_DEFAULT = sf::Color::White;
+const std::string WINDOW_TITLE_DEFAULT = "Alabaster";
+
+class DisplayManager :
+    public Manager
+{
+public:
+    ~DisplayManager();
+
+    static DisplayManager& getInstance();
+
+    bool drawString(sf::Vector2f pos, std::string str, Justification just, sf::Color color) const;
+
+    virtual void update();
+
+    sf::RenderWindow* getWindow() const;
+    int getWidth() const;
+    int getHeight() const;
+    sf::Color getBgColor() const;
+    void setBgColor(sf::Color newColor);
+    bool setDefaultFont(std::string fontFile);
+private:
+    DisplayManager(); //Singleton - Le constructeur est privé
+    DisplayManager(const DisplayManager&) = delete; //Singleton - pas de copie
+    DisplayManager& operator=(const DisplayManager&) = delete; //Singleton - pas d'assignation
+
+    sf::RenderWindow* pWindow;
+    int windowWidth;
+    int windowHeight;
+    sf::Color bgColor;
+    sf::Font defaultFont;
+
+    sf::CircleShape testCircle;
+};
+
