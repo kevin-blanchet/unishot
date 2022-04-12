87635f269e31f0f0c960704e9fec778729329b12
kevin-blanchet <kevin.blanchet@ynov.com>
2022-04-12 13:45:51 +0200

    modified main to test game manager

--- sfml-engine/main.cpp
+++ sfml-engine/main.cpp
@@ -1,27 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include "LogManager.h"
+#include "GameManager.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
     LM.consoleLog("Begin play");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    GM.update();
 
     LM.consoleLog("End play");
 
